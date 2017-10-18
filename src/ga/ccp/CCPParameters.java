ef93eb6e2680a6d5b632b45b90d210072ba213a0
Luan Gustavo Bicesto <luanb@LINUXCIT010905.openair4G.eur>
2017-10-17 23:05:16 -0200

    good results

--- src/ga/ccp/CCPParameters.java
+++ src/ga/ccp/CCPParameters.java
@@ -27,13 +27,13 @@ public class CCPParameters {
     }
     public static final boolean ENABLE_ONE_CHANGE = true;
     public static final boolean ENABLE_SWAP = true;
-    public static final boolean ENABLE_TRIPLE_SWAP = true;
+    public static final boolean ENABLE_TRIPLE_SWAP = false;
     public static final boolean ENABLE_QUADRUPLE_SWAP = false;
-    public static final double MAX_PERCENTAGE_NUMBER_NODES_LS_SWAP = 0.025;
-    public static final double MAX_PERCENTAGE_NUMBER_NODES_LS_ONE_CHANGE = 0.1;
+    public static final double MAX_PERCENTAGE_NUMBER_NODES_LS_SWAP = 0.01;
+    public static final double MAX_PERCENTAGE_NUMBER_NODES_LS_ONE_CHANGE = 0.02;
     public static final int NUMBER_TRIPLE_SWAP_EXECUTIONS = 1;
     public static final int NUMBER_QUADRUPLE_SWAP_EXECUTIONS = 10;
-    public static final boolean BEST_IMPROVING = true;
+    public static final boolean BEST_IMPROVING = false;
     public static final boolean ONE_NODE_OPTION = false;
     /*Local Search*/
     
--- src/ga/framework/GAConfiguration.java
+++ src/ga/framework/GAConfiguration.java
@@ -4,14 +4,14 @@ public class GAConfiguration {
     public static final long TOTAL_RUNNING_TIME = 60;
     public static final int NUMBER_GENERATIONS = 192000;
     public static final int POPULATION_SIZE = 100;
-    public static final double MUTATION_RATE = 1.2 / (double)POPULATION_SIZE;
+    public static final double MUTATION_RATE = 0.5 / (double)POPULATION_SIZE;
     public static boolean ENABLE_MUTATION_SWAP = false;
     
     public static boolean ENABLE_HYBRID_POPULATION = true;
     public static boolean ENABLE_LS_BEST_CHROMOSOME_OFFSPRINGS = false;
     public static int PERCENTAGE_HYBRID_POPULATION = 10; //not used
-    public static int ABSOLUTE_HYBRID_POPULATION = 20;
-    public static double PERCENTAGE_APPLY_HYBRID_TRANSFORMATION = 0.5;
+    public static int ABSOLUTE_HYBRID_POPULATION = 70;
+    public static double PERCENTAGE_APPLY_HYBRID_TRANSFORMATION = 0.6;
     
     public static double PERCENTAGE_APPLY_ORIGINAL_LOCAL_SEARCH = 0.1;
     public static int ABSOLUTE_ORIGINAL_LOCAL_SEARCH_POPULATION = 10;
@@ -21,7 +21,7 @@ public class GAConfiguration {
     public static final boolean ENABLE_NEW_POPULATION = true;
     public static final boolean ENABLE_MUTATION = true;
     public static final double TIMES_SIZE_NEW_POPULATION = 2; //not used
-    public static double PERCENTAGE_APPLY_NEW_POPULATION = 0.1;
+    public static double PERCENTAGE_APPLY_NEW_POPULATION = 0.5;
     /*New population*/
     
     /*Not used*/
